eeb903bba225dc45186b9445743646c6c7afb13d
Djordje Momirov <dj.momirov@gmail.com>
2023-12-14 00:29:35 +0100

    Fixed typo 2

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -91,7 +91,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
             <Link
-              page="Contact Us"
+              page="Contact Me"
               selectedPage={selectedPage}
               setSelectedPage={setSelectedPage}
             />
